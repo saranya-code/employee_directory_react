7c276d159524e4acd95ef8c666ca25fad3b1ec08
saranya-code <saranyadayalan28@gmail.com>
2021-05-07 22:08:31 -0500

    Added EmployeeList component & worked on name sort

--- src/components/Employee.js
+++ src/components/Employee.js
@@ -1,35 +1,74 @@
-import React,{ Component } from "react";
+import React, { Component } from "react";
 import axios from "axios"
+import EmployeeList from "./EmployeeList"
+import _ from 'lodash'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCaretUp, faCaretDown } from '@fortawesome/free-solid-svg-icons'
+
 class Employee extends Component{
 
     state= {
-        employees:[]
+        employees:null,
+        filteredEmployees:null,
+        sortOrder:'asc'
     }
-
+    
     getEmployeeDetails = () => {
         return axios.get('https://randomuser.me/api/?results=10&inc=phone,name,email,picture')
         .then(response => {
-            this.setState({employees:response.data.results})
+            this.setState({
+                    employees:response.data.results
+                    .map(result => {
+                        return {
+                            picture: result.picture.thumbnail,
+                            name: `${result.name.first} ${result.name.last}`,
+                            email: result.email,
+                            phone: result.phone
+                        }
+                    })
+                })
         })
     }
 
     componentDidMount(){
         this.getEmployeeDetails();
     }
+
+    sortEmployee = () => {
+        if(this.state.filteredEmployees !== null){
+            this.setState({
+                filteredEmployees:_.orderBy((this.state.filteredEmployees), ['name'], [this.state.sortOrder]),
+                sortOrder: this.state.sortOrder === "asc" ? "desc" : "asc"
+                
+            }
+        )
+        } else {
+            this.setState({
+                employees:_.orderBy((this.state.employees), ['name'], [this.state.sortOrder]),
+                sortOrder: this.state.sortOrder === "asc" ? "desc" : "asc"
+            }
+        )
+        }
+        
+    }
+
     handleInputChange = event =>{
         const searchValue= event.target.value
-        console.log(searchValue)
-        const filteredEmployee = this.state.employees.filter(employee => {
-           if(employee.name.first.toLowerCase().includes(searchValue)){
-               return employee
-           }
+        if(searchValue){
+            const filteredEmployee = this.state.employees.filter(employee => {
+            if(employee.name.toLowerCase().includes(searchValue)){
+                return employee
+            }
         })
-        this.setState({employees:filteredEmployee})
-        // console.log(filteredEmployee)
+        this.setState({filteredEmployees:filteredEmployee})
+        } else {
+            this.setState({filteredEmployees:null})
+        }
+        
     }
 
     render(){
-        console.log('employeeList', this.state.employees)
+        // console.log('employeeList', this.state.employees)
         return(
             <div>
                 <div class="text-center">
@@ -38,26 +77,25 @@ class Employee extends Component{
                     <br/>
                 </div>
            
-            <table class="table">
-                <thead class="thead-dark">
-                    <tr>
-                        <th scope="col">Image</th>
-                        <th scope="col">Name</th>
-                        <th scope="col">Email</th>
-                        <th scope="col">Phone</th>
-                    </tr>
-                </thead>
-                
-                    {this.state.employees.length ? this.state.employees.map(employee => 
+                <table class="table">
+                    <thead class="thead-dark">
                         <tr>
-                            <td> <img src ={employee.picture.thumbnail} alt= "thumbnail"/> </td>
-                            <td>{employee.name.first} {employee.name.last}</td>
-                            <td>{employee.email}</td>
-                            <td>{employee.phone}</td>
-                        </tr>)
-                        : <div>No Employee to show</div>
-                    }
-            </table>
+                            <th>Image</th>
+                            <th>Name <button className="sort" onClick={this.sortEmployee}> 
+                            {this.state.sortOrder==="asc" && <FontAwesomeIcon icon={faCaretUp} />}
+                            {this.state.sortOrder==="desc" && <FontAwesomeIcon icon={faCaretDown} />}
+                            </button>
+                            </th>
+                            <th>Email</th>
+                            <th>Phone</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {(this.state.employees || this.state.filteredEmployees) ?
+                         <EmployeeList employees={this.state.filteredEmployees || this.state.employees} />
+                        : <tr><td colSpan="4">No Data</td></tr>}
+                    </tbody>
+                </table>
             </div>
         );
     }
